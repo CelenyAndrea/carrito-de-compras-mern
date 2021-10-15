95392b72badb34983e2e00c55435b3be32e756cb
CelenyAndrea <77702240+CelenyAndrea@users.noreply.github.com>
2021-10-15 08:00:11 -0500

    ok

--- backend/script.js
+++ backend/script.js
@@ -8,19 +8,19 @@ const Product = require("./models/Product");
 
 connectDB();
 
-const importData = async () => {
-  try {
-    await Product.deleteMany({});
+// const importData = async () => {
+//   try {
+//     await Product.deleteMany({});
 
-    await Product.insertMany(productData);
+//     await Product.insertMany(productData);
 
-    console.log("Data Import Success");
+//     console.log("Data Import Success");
 
-    process.exit();
-  } catch (error) {
-    console.error("Error with data import", error);
-    process.exit(1);
-  }
-};
+//     process.exit();
+//   } catch (error) {
+//     console.error("Error with data import", error);
+//     process.exit(1);
+//   }
+// };
 
-importData();  
+// importData();  
