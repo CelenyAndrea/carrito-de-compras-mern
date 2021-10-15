f38f573de65f426941009915a7e561c15973d113
CelenyAndrea <77702240+CelenyAndrea@users.noreply.github.com>
2021-10-15 18:35:51 -0500

    Start react-redux

--- frontend/src/components/SideDrawer.js
+++ frontend/src/components/SideDrawer.js
@@ -13,7 +13,7 @@ const SideDrawer = ({show, click}) => {
         <div className={sidedrawerClass.join(' ')}>
             <ul className="sidedrawer__links" onClick={click}>
                 <li>
-                    <Link>
+                    <Link to="/cart">
                     <i className="fas fa-shopping-cart"></i>
                     <span>
                         Carito de compra
